bfdb3df39378c7c64f6ec92e8d1ac78308d4593f
TetianaKlitna <146033253+TetianaKlitna@users.noreply.github.com>
2024-02-23 11:41:40 -0800

    Add files via upload

---
+++ probability_uniform_cdf.py
@@ -0,0 +1,31 @@
+# Min and max wait times for back-up that happens every 30 min
+min_time = 0
+max_time = 30
+
+# Import uniform from scipy.stats
+from scipy.stats import uniform
+
+# Calculate probability of waiting less than 5 mins
+prob_less_than_5 = uniform.cdf(5, min_time, max_time)
+print(prob_less_than_5)
+
+# Calculate probability of waiting more than 5 mins
+prob_greater_than_5 = 1 - uniform.cdf(5, min_time, max_time)
+print(prob_greater_than_5)
+
+# Calculate probability of waiting 10-20 mins
+prob_between_10_and_20 = uniform.cdf(20, min_time, max_time) - uniform.cdf(10, min_time, max_time)
+print(prob_between_10_and_20)
+
+# Set random seed to 334
+np.random.seed(334)
+
+# Import uniform
+from scipy.stats import uniform
+
+# Generate 1000 wait times between 0 and 30 mins
+wait_times = uniform.rvs(0, 30, size=1000)
+
+# Create a histogram of simulated times and show plot
+plt.hist(wait_times)
+plt.show()
