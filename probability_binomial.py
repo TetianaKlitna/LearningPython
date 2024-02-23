17e629ac7aacef07bdb653d83a92d3ded98f0aac
TetianaKlitna <146033253+TetianaKlitna@users.noreply.github.com>
2024-02-23 12:47:43 -0800

    Update probability_binomial.py

--- probability_binomial.py
+++ probability_binomial.py
@@ -24,6 +24,18 @@ print(prob_3)
 prob_less_than_or_equal_1 = binom.cdf(1, 3, 0.3)
 print(prob_less_than_or_equal_1)
 
+# Expected number won with 30% win rate
+won_30pct = 3 * 0.3
+print(won_30pct)
+
+# Expected number won with 25% win rate
+won_25pct = 3 * 0.25
+print(won_25pct)
+
+# Expected number won with 35% win rate
+won_35pct = 3 * 0.35
+print(won_35pct)
+
 # Probability of closing > 1 deal out of 3 deals
 prob_greater_than_1 = 1 - binom.cdf(1, 3, 0.3)
 print(prob_greater_than_1)
